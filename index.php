f318f3d78683710695c37755e7342df97de9f4cd
Claudiu Ceia <claudiu.ceia@gmail.com>
2014-01-30 01:02:46 -0800

    Bootstrap

--- index.php
+++ index.php
@@ -1,24 +1,65 @@
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
-    <title>SkullBot - http://skullbox.info IRC bot</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
+    <meta name="description" content="IRC bot for http://skullbox.info">
+    <meta name="author" content="Claudiu Ceia">
+    <title>SkullBot - http://skullbox.info IRC bot</title>
+
+    <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!-- Custom styles for this template -->
+    <link href="jumbotron-narrow.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <div class="container">
+      <div class="header">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="#">Home</a></li>
+        </ul>
+        <h3 class="text-muted">SkullBot</h3>
+      </div>
+      <div class="jumbotron">
+        <h1>SkullBot</h1>
+        <p class="lead">SkullBot is the beginning of the SkullBox.info Skynet project, due to become self-aware in the summer of 2017. I kid, I kid, it's a dumb IRC bot that does some web scraping and posting and replying to basic queries. Nothing to see here (unless you own a SMF forum or are a <a href="http://skullbox.info">SkullBox.info</a> member.
+        <p><a class="btn btn-lg btn-success" href="irc://chat.freenode.net:6667/skullbox" role="button">#skullbox</a></p>
+      </div>
+      <div class="row marketing">
+        <div class="col-lg-6">
+          <h4>New threads</h4>
+          <p>SkullBot will spam the crap out of your IRC channel by posting titles and links to the newest topics (considering all threads too, unsure).</p>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
+          <h4>Search</h4>
+          <p>Wouldn't it be awesome to be able to search for stuff on your forum from the comfort of &lt;insert IRC client name here&gt;. Yes. It would.</p>
+        </div>
+
+        <div class="col-lg-6">
+          <h4>Forum down?</h4>
+          <p>SkullBot will yell on your channel if your forum is down (no pressure). </p>
+
+          <h4>Reply from IRC</h4>
+          <p>I'm just making this up as I go, but it would be nice if SkullBot allowed you to reply to stuff on the forum (requires deeper integration).</p>
+        </div>
+      </div>
+
+      <div class="footer">
+        <p>&copy; Claudiu Ceia 2013</p>
+      </div>
+
+    </div> <!-- /container -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <!-- Placed at the end of the document so the pages load faster -->
   </body>
 </html>
