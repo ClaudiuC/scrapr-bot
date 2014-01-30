c641cded5a705b836dce8e0e0701f5cdf5fd063f
Claudiu Ceia <claudiu.ceia@gmail.com>
2014-01-30 03:57:08 -0800

    Basic exception logging

--- crawler/process.php
+++ crawler/process.php
@@ -2,5 +2,11 @@
 
 require_once 'CrawlerTask.php';
 
+set_exception_handler(
+  function(Exception $ex) {
+    error_log($ex->getMessage());
+  }
+);
+
 $task = new CrawlerTask($argv[0]);
 $task->setXpath(CrawlerPath::get('/rss/channel/item/'))->fetch()->store();
